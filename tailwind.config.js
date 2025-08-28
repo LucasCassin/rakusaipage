8427b97f21e89ad6cc48f81683f31998ee200d70
Lucas Cassin <lucas.cassin@outlook.com>
2025-08-28 02:07:21 +0000

    build: add `@tailwindcss/typography` lib, change `^` from others lib, update `tailwind config`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // Adicionando a paleta de cores customizada
+      colors: {
+        "rakusai-yellow-light": "#ffd800",
+        "rakusai-yellow-dark": "#f7bf22",
+        "rakusai-pink": "#e40788",
+        "rakusai-purple": "#b000b0",
+      },
+      // Adicionando a fonte Poppins como padrão
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [require("tailwind-scrollbar")],
+  // Adicionando os dois plugins
+  plugins: [require("tailwind-scrollbar"), require("@tailwindcss/typography")],
 };
