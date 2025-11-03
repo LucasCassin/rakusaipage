e2c51690c96ba8788118580ca0c016428965bd15
Lucas Cassin <lucas.cassin@outlook.com>
2025-11-03 23:20:57 +0000

    feat: add `read:presentation:self` default feature to existing users

---
+++ infra/migrations/1762211961456_update-admin-presentation-features.js
@@ -0,0 +1,34 @@
+// Lista de features a serem adicionadas
+const featuresToAdd = [
+  "create:presentation",
+  "read:presentation:self",
+  "read:presentation:other",
+  "update:presentation",
+  "delete:presentation",
+  "manage:presentation_viewers",
+  "create:scene",
+  "update:scene",
+  "delete:scene",
+  "create:scene_element",
+  "update:scene_element",
+  "delete:scene_element",
+  "manage:element_types",
+];
+
+exports.up = (pgm) => {
+  // Converte o array de features para o formato SQL ARRAY['feature1', 'feature2']
+  const featuresSqlArray = `ARRAY[${featuresToAdd.map((f) => `'${f}'`).join(", ")}]`;
+
+  pgm.sql(`
+    UPDATE users
+    SET
+      features = (
+        SELECT array_agg(DISTINCT feature)
+        FROM unnest(features || ${featuresSqlArray}) AS t(feature)
+      )
+    WHERE
+      username = 'mainUser';
+  `);
+};
+
+exports.down = false;
